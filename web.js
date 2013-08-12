4ddb199a727d079847a91dcd0264fee85c3350e1
Hesham Al-Zaemey <hzaemey@gmail.com>
2013-08-12 09:44:02 +0000

    Fixed to connect on port 8080. previous version did not seem to connect when using the veriable htmlfile.

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
 var express = require('express');
-var fs -= require('fs');
-var htmlfile = "index.html";
+var fs = require('fs');
+//var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-
 app.get('/', function(request, response) {
-  var html = fs.readFileSync('htmlfile').toString();
+  var html = fs.readFileSync('index.html').toString();
   response.send(html);
 });
 
